46ebcdba3f1d27d742de5f22ddad51569805bc2d
Emon <emon@kaz-software.com>
2024-03-13 00:46:24 +0600

    Frontend: Navbar is formed and some CSS styles are also added.

--- Frontend/e-commerce/src/app/navbar/navbar.component.ts
+++ Frontend/e-commerce/src/app/navbar/navbar.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  logoImagePath = "https://i.pinimg.com/originals/cb/ca/38/cbca38012ddfa7faaab7591df95c2b5a.png";
 }
