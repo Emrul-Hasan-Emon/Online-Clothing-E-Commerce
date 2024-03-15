0b2f08a2bc368f8b9addc012c2cef224694dd44b
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-03-15 11:55:28 +0600

    Backend: Database configuration is done.

---
+++ Backend-Go/database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"database/sql"
+	"net/url"
+
+	"github.com/Emrul-Hasan-Emon/repositories/ecommerce/config"
+	log "github.com/Emrul-Hasan-Emon/repositories/ecommerce/log4u"
+)
+
+type Database struct {
+	db *sql.DB
+}
+
+func CreateNewDatabaseInstance(dbDef *config.DbDef) *Database {
+	return &Database{openDatabase(dbDef)}
+}
+
+func openDatabase(dbDef *config.DbDef) *sql.DB {
+	databaseUrl := dbDef.User + ":" + url.PathEscape(
+		dbDef.Password,
+	) + "@tcp(" + dbDef.Host + ")/" + dbDef.DatabaseName
+
+	database, err := sql.Open("mysql", databaseUrl)
+	if err != nil {
+		log.Fatalf("Failed to open database %s: %s: [%v]", dbDef.Host, dbDef.DatabaseName, err)
+	}
+	log.Debugf("Connected to database %s: %s", dbDef.Host, dbDef.DatabaseName)
+	err = database.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping database %s: %s: [%v]", dbDef.Host, dbDef.DatabaseName, err)
+	}
+	log.Debugf("Database available %s:%s", dbDef.Host, dbDef.DatabaseName)
+	return database
+}
+
+func (db *Database) Close() {
+	db.db.Close()
+}
