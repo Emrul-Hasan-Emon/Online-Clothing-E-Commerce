9f1491933a10a799b146c079f7b6d4e79c3d6cb5
Emon <emon@kaz-software.com>
2024-03-17 15:55:22 +0600

    Frontend: Modal added for Men and Women.

---
+++ Frontend/e-commerce/src/app/Data/nav-content.ts
@@ -0,0 +1,96 @@
+export const navigation = {
+    Women: {
+        id: 'women',
+        name: 'Women',
+        featured: [
+            {
+                name: 'A',
+                href: '/',
+                imageSrc: "",
+                imageAlt: ""
+            },
+        ],
+        sections: [
+            {
+                id: 'clothing',
+                name: 'Clothing',
+                items: [
+                    { name: 'A', id: 'a', href: "" },
+                    { name: 'B', id: 'b', href: "" },
+                    { name: 'C', id: 'c', href: "" },
+                    { name: 'D', id: 'd', href: "" },
+                    { name: 'E', id: 'e', href: "" }
+                ]
+            },
+            {
+                id: 'accessories',
+                name: 'Accessories',
+                items: [
+                    { name: 'A', id: 'a', href: "" },
+                    { name: 'B', id: 'b', href: "" },
+                    { name: 'C', id: 'c', href: "" },
+                    { name: 'D', id: 'd', href: "" },
+                    { name: 'E', id: 'e', href: "" }
+                ]
+            },
+            {
+                id: 'brands',
+                name: 'Brands',
+                items: [
+                    { name: 'A', id: 'a', href: "" },
+                    { name: 'B', id: 'b', href: "" },
+                    { name: 'C', id: 'c', href: "" },
+                    { name: 'D', id: 'd', href: "" },
+                    { name: 'E', id: 'e', href: "" }
+                ]
+            }
+        ]
+    },
+    Men: {
+        id: 'women',
+        name: 'Women',
+        featured: [
+            {
+                name: 'A',
+                href: '/',
+                imageSrc: "",
+                imageAlt: ""
+            },
+        ],
+        sections: [
+            {
+                id: 'clothing',
+                name: 'Clothing',
+                items: [
+                    { name: 'A', id: 'a', href: "" },
+                    { name: 'B', id: 'b', href: "" },
+                    { name: 'C', id: 'c', href: "" },
+                    { name: 'D', id: 'd', href: "" },
+                    { name: 'E', id: 'e', href: "" }
+                ]
+            },
+            {
+                id: 'accessories',
+                name: 'Accessories',
+                items: [
+                    { name: 'A', id: 'a', href: "" },
+                    { name: 'B', id: 'b', href: "" },
+                    { name: 'C', id: 'c', href: "" },
+                    { name: 'D', id: 'd', href: "" },
+                    { name: 'E', id: 'e', href: "" }
+                ]
+            },
+            {
+                id: 'brands',
+                name: 'Brands',
+                items: [
+                    { name: 'A', id: 'a', href: "" },
+                    { name: 'B', id: 'b', href: "" },
+                    { name: 'C', id: 'c', href: "" },
+                    { name: 'D', id: 'd', href: "" },
+                    { name: 'E', id: 'e', href: "" }
+                ]
+            }
+        ]
+    },
+}
--- Frontend/e-commerce/src/app/navbar/navbar-content/navbar-content.component.ts
+++ Frontend/e-commerce/src/app/navbar/navbar-content/navbar-content.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { navigation } from 'src/app/Data/nav-content';
 
 @Component({
   selector: 'app-navbar-content',
   templateUrl: './navbar-content.component.html',
   styleUrls: ['./navbar-content.component.css']
 })
-export class NavbarContentComponent {
+export class NavbarContentComponent implements OnInit {
   catergory: any;
   @Input() // The input will come from NavBar Component
   selectedCategory: any;
+
+  ngOnInit(): void {
+      this.catergory = navigation
+      console.log("Selected Section: ", this.selectedCategory);
+      
+  }
 }
