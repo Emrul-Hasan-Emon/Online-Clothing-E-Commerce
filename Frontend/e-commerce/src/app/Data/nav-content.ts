9e1b9463477088e205d4c976a844572dbc38c334
Emon <emon@kaz-software.com>
2024-03-17 17:11:01 +0600

    Frontend: Added some data for Men and Women modal in homepage.

--- Frontend/e-commerce/src/app/Data/nav-content.ts
+++ Frontend/e-commerce/src/app/Data/nav-content.ts
@@ -15,22 +15,28 @@ export const navigation = {
                 id: 'clothing',
                 name: 'Clothing',
                 items: [
-                    { name: 'A', id: 'a', href: "" },
-                    { name: 'B', id: 'b', href: "" },
-                    { name: 'C', id: 'c', href: "" },
-                    { name: 'D', id: 'd', href: "" },
-                    { name: 'E', id: 'e', href: "" }
+                    { name: 'Borkha', id: 'borkha', href: "" },
+                    { name: 'Tops', id: 'tops', href: "" },
+                    { name: 'Dresses', id: 'dresses', href: "" },
+                    { name: 'Pants', id: 'pants', href: "" },
+                    { name: 'Sweaters', id: 'sweaters', href: "" },
+                    { name: 'T-Shirts', id: 't-shirts', href: "" },
+                    { name: 'Jackets', id: 'jackets', href: "" },
+                    { name: 'Gouns', id: 'gouns', href: "" },
+                    { name: 'Sarees', id: 'sarees', href: "" },
+                    { name: 'Kurtas', id: 'kurtas', href: "" }
                 ]
             },
             {
                 id: 'accessories',
                 name: 'Accessories',
                 items: [
-                    { name: 'A', id: 'a', href: "" },
-                    { name: 'B', id: 'b', href: "" },
-                    { name: 'C', id: 'c', href: "" },
-                    { name: 'D', id: 'd', href: "" },
-                    { name: 'E', id: 'e', href: "" }
+                    { name: 'Watches', id: 'watches', href: "" },
+                    { name: 'Walletes', id: 'walletes', href: "" },
+                    { name: 'Bags', id: 'bags', href: "" },
+                    { name: 'Sunglasses', id: 'sunglasses', href: "" },
+                    { name: 'Hats', id: 'hats', href: "" },
+                    { name: 'Belts', id: 'belts', href: "" }
                 ]
             },
             {
@@ -62,22 +68,25 @@ export const navigation = {
                 id: 'clothing',
                 name: 'Clothing',
                 items: [
-                    { name: 'A', id: 'a', href: "" },
-                    { name: 'B', id: 'b', href: "" },
-                    { name: 'C', id: 'c', href: "" },
-                    { name: 'D', id: 'd', href: "" },
-                    { name: 'E', id: 'e', href: "" }
+                    { name: 'Kurtas', id: 'kurtas', href: "" },
+                    { name: 'Shirt', id: 'shirt', href: "" },
+                    { name: 'Pants', id: 'pants', href: "" },
+                    { name: 'Sweaters', id: 'sweaters', href: "" },
+                    { name: 'T-Shirts', id: 't-shirts', href: "" },
+                    { name: 'Jackets', id: 'jackets', href: "" },
+                    { name: 'Activewear', id: 'activewear', href: "" }
                 ]
             },
             {
                 id: 'accessories',
                 name: 'Accessories',
                 items: [
-                    { name: 'A', id: 'a', href: "" },
-                    { name: 'B', id: 'b', href: "" },
-                    { name: 'C', id: 'c', href: "" },
-                    { name: 'D', id: 'd', href: "" },
-                    { name: 'E', id: 'e', href: "" }
+                    { name: 'Watches', id: 'watches', href: "" },
+                    { name: 'Walletes', id: 'walletes', href: "" },
+                    { name: 'Bags', id: 'bags', href: "" },
+                    { name: 'Sunglasses', id: 'sunglasses', href: "" },
+                    { name: 'Hats', id: 'hats', href: "" },
+                    { name: 'Belts', id: 'belts', href: "" }
                 ]
             },
             {
