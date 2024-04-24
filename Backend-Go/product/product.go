78a03ede512a94849ecb34dac49b9f058fbe6789
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-04-25 05:15:13 +0600

    Backend: Handler for /products/{id} is added.

--- Backend-Go/product/product.go
+++ Backend-Go/product/product.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/Emrul-Hasan-Emon/repositories/ecommerce/common"
 	"github.com/Emrul-Hasan-Emon/repositories/ecommerce/database"
 	log "github.com/Emrul-Hasan-Emon/repositories/ecommerce/log4u"
+	"github.com/Emrul-Hasan-Emon/repositories/ecommerce/model"
 )
 
 type Product struct {
@@ -31,8 +33,36 @@ func (pr *Product) CreateProductFetcher(
 			return
 		}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
+		common.SetHeader(w)
+		w.Write(productJSON)
+	}
+}
+
+func (pr *Product) CreateSingleProductFetcher(
+	db *database.Database,
+) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rw := common.RequestWrapper(r)
+		productId, err := rw.FindProductId()
+
+		if err != nil {
+			log.Errorf("error to fetch start index. Error: %s", err.Error())
+			return
+		}
+
+		respone, err := db.FetchProductByID(model.ProductID(productId))
+		if err != nil {
+			log.Errorf("error to parse products. Error: %s", err.Error())
+			return
+		}
+		// Convert products slice to JSON
+		productJSON, err := json.Marshal(respone)
+		if err != nil {
+			log.Errorf("unexpected erroc occured. Error: %s", err)
+			return
+		}
+
+		common.SetHeader(w)
 		w.Write(productJSON)
 	}
 }
