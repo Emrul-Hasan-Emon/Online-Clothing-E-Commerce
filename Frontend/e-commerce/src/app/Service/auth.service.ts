15ef2b4d48c2aa337d81b44063d16e606bc6da38
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-04-29 04:41:26 +0600

    Frontend: Auth service is created.

---
+++ Frontend/e-commerce/src/app/Service/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl = 'auth';
+  public isUserLogged = false;
+  public isAdminLogged = false;
+  
+  constructor(private http: HttpClient) { }
+
+  public registerNewUser(registerUserData: any) {
+    return this.http.post(`${this.baseUrl}/register`, registerUserData)
+  }
+
+  public authenticateUser(userData: any) {
+    return this.http.post(`${this.baseUrl}/login`, userData)
+  }
+}
