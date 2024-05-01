2bc83df3092fa165272c7452cf01a32c0f919254
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-05-01 22:21:38 +0600

    Backend: Modified category.sql.

--- Backend-Go/env/category.sql
+++ Backend-Go/env/category.sql
@@ -7,29 +7,27 @@ CREATE TABLE Category (
 );
 
 
+
 -- Inserting Some Category for Female
-INSERT INTO Category (CategoryID, CategoryName, Gender)
-VALUES ('borkha', 'Borkha', 'Female');
-
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('tops', 'Tops', 'Female');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('pants', 'Pants', 'Female');
-
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('Sweaters', 'Sweaters', 'Female');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('t-Shirts', 'T-Shirts', 'Female');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('jackets', 'Jackets', 'Female');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('gouns', 'Gouns', 'Female');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('sarees', 'Sarees', 'Female');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('Kurtas', 'Kurtas', 'Female');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('borkha', 'Borkha', 'Women');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('tops', 'Tops', 'Women');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('pants', 'Pants', 'Women');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('Sweaters', 'Sweaters', 'Women');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('t-Shirts', 'T-Shirts', 'Women');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('jackets', 'Jackets', 'Women');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('gouns', 'Gouns', 'Women');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('sarees', 'Sarees', 'Women');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('Kurtas', 'Kurtas', 'Women');
 
 -- Inserting Some Category for Male
 
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('kurtas', 'Kurtas', 'Male');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('shirt', 'Shirt', 'Male');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('pants', 'Pants', 'Male');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('sweaters', 'Sweaters', 'Male');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('t-shirts', 'T-Shirts', 'Male');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('jackets', 'Jackets', 'Male');
-INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('activewear', 'Activewear', 'Male');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('kurtas', 'Kurtas', 'Men');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('shirt', 'Shirt', 'Men');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('pants', 'Pants', 'Men');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('sweaters', 'Sweaters', 'Men');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('t-shirts', 'T-Shirts', 'Men');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('jackets', 'Jackets', 'Men');
+INSERT INTO Category (CategoryID, CategoryName, Gender) VALUES ('activewear', 'Activewear', 'Men');
 
 -- See everything from category details
 SELECT * FROM Category;
