03c08f8c88133ee2a78027a3aa2a658ea39bce4f
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-05-01 17:09:01 +0600

    Frontend: Modified add product page.

--- Frontend/e-commerce/src/app/Modules/admin/add-product/add-product.component.ts
+++ Frontend/e-commerce/src/app/Modules/admin/add-product/add-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -9,6 +9,12 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AddProductComponent implements OnInit{
   addProductForm: FormGroup;
   selectedImage;
+  sizesOptions: string[] = ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
+  colorOptions: string[] = ['Black', 'White', 'Maroon', 'Orange', 'Yellow'];
+  selectedPairs: { size: string, color: string }[] = [];
+
+  availableSizeOptions = this.sizesOptions;
+  availableColorOptions = this.colorOptions;
 
   ngOnInit(): void {
     this.addProductForm = new FormGroup({
@@ -21,7 +27,8 @@ export class AddProductComponent implements OnInit{
         sizes: new FormArray([
           this.createSizeFormGroup()
         ]),
-        gender: new FormControl('', Validators.required)
+        gender: new FormControl('', Validators.required),
+        image: new FormControl('', Validators.required)
       });
   }
 
@@ -50,6 +57,53 @@ export class AddProductComponent implements OnInit{
         // console.log('Selected Image Url: ', this.selectedImage);
       };
       reader.readAsDataURL(inputElement.files[0]);
+    }
+  }
+
+  productDetailsSubmitted() {
+    console.log("New Product Details ----> ", this.addProductForm.value);
+  }
+
+  isSizeColorPairSelected(size: string, color: string): boolean {
+    // console.log(`Size : ${size}, Color: ${color}`);
+    return this.selectedPairs.some(pair => pair.size === size && pair.color === color);
+  }
+
+  getColorControl(index: number): AbstractControl {
+    const sizesArray = this.addProductForm.get('sizes') as FormArray;
+    const sizeGroup = sizesArray.at(index) as FormGroup;
+    return sizeGroup.get('color');
+}
+
+  getSizeControl(index: number): AbstractControl {
+      const sizesArray = this.addProductForm.get('sizes') as FormArray;
+      const sizeGroup = sizesArray.at(index) as FormGroup;
+      return sizeGroup.get('size');
+  }
+
+  insertSizeColorPair(index: number) {
+    const sizesArray = this.addProductForm.get('sizes') as FormArray;
+    const sizeGroup = sizesArray.at(index) as FormGroup;
+    const size = sizeGroup.get('size').value;
+    const color = sizeGroup.get('color').value;
+
+    // console.log(`Index: ${index}, Size: ${size}, Color: ${color}`);
+    if(size && color) {
+      this.selectedPairs.push({size, color});
+    }
+  }
+
+  removeSizeColorPair(index: number) {
+    const sizesArray = this.addProductForm.get('sizes') as FormArray;
+    const sizeGroup = sizesArray.at(index) as FormGroup;
+    const size = sizeGroup.get('size').value;
+    const color = sizeGroup.get('color').value;
+
+    // Find the index of the pair in the selectedPairs array
+    const pairIndex = this.selectedPairs.findIndex(pair => pair.size === size && pair.color === color);
+
+    if (pairIndex !== -1) {
+      this.selectedPairs.splice(pairIndex, 1);
     }
   }
 }
