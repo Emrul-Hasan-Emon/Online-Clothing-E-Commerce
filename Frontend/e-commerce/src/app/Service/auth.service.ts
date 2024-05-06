e49d5ba2e2be1300224bfd55ffbec4b7f254e8b9
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-05-06 23:12:43 +0600

    Frontend: Auth Service Modified.

--- Frontend/e-commerce/src/app/Service/auth.service.ts
+++ Frontend/e-commerce/src/app/Service/auth.service.ts
@@ -9,11 +9,28 @@ export class AuthService {
   private baseUrl = 'auth';
   public isUserLogged = false;
   public isAdminLogged = false;
+  public isDeliveryLogged = false;
   public userInfo: any;
 
   public loginCredentials = new BehaviorSubject<any>(null);
+  public adminLoginAlert = new BehaviorSubject<Boolean>(null);
 
   constructor(private http: HttpClient) { }
+
+  public adminLoggedIn(adminData: any) {
+    this.isAdminLogged = true;
+    this.validationHappened(adminData);
+  }
+
+  public userLoggedIn(userData: any) {
+    this.isUserLogged = true;
+    this.validationHappened(userData);
+  }
+
+  public deliveryManLoggedIn(deliveryManData: any) {
+    this.isDeliveryLogged = true;
+    this.validationHappened(deliveryManData);
+  }
 
   public registerNewUser(registerUserData: any) {
     return this.http.post(`${this.baseUrl}/register`, registerUserData)
