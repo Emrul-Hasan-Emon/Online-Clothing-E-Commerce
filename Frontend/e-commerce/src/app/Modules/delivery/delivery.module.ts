bef4dde28c201ad9d5c2bf16a7cb3ded29532bba
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-05-06 23:14:24 +0600

    Frontend: Delivery module created and component functionalities are added some.

---
+++ Frontend/e-commerce/src/app/Modules/delivery/delivery.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DeliveryHomeComponent } from './delivery-home/delivery-home.component';
+
+
+
+@NgModule({
+  declarations: [
+    DeliveryHomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DeliveryModule { }
