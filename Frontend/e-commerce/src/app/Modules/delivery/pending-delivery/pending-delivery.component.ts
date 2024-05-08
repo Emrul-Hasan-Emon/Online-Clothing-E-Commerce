af2cf3ccc22ce4be22244100eee9e42191116b0a
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-05-08 10:30:21 +0600

    Frontend: Pending Delivery Order details are shown in the UI.

--- Frontend/e-commerce/src/app/Modules/delivery/pending-delivery/pending-delivery.component.ts
+++ Frontend/e-commerce/src/app/Modules/delivery/pending-delivery/pending-delivery.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DeliveryService } from 'src/app/Service/Delivery/delivery.service';
+import { OrderService } from 'src/app/Service/Order/order.service';
 import { AuthService } from 'src/app/Service/auth.service';
 
 @Component({
@@ -11,17 +12,33 @@ export class PendingDeliveryComponent implements OnInit {
   flag: boolean = false;
   pendingDeliveryDetails;
   userDetails;
+  orderDetails;
 
   constructor(
     private deliveryService: DeliveryService,
-    private authService: AuthService
+    private authService: AuthService,
+    private orderService: OrderService
   ) {}
   
+  private fetchOrderForPendingDelivery() {
+    this.orderService.fetchAllOrders().subscribe(
+      (orders: any) => {
+        console.log("All orders: ", orders);
+        var orderIds = [];
+        for (let idx = 0; idx < this.pendingDeliveryDetails.length; idx++) {
+          orderIds.push(this.pendingDeliveryDetails[idx].orderID);
+        }
+        this.orderDetails = orders.filter(order => orderIds.includes(order.orderID));
+        console.log("Filtered Orders: ", this.orderDetails);
+      }
+    )
+  }
   private fetchPendingDelivery() {
     this.deliveryService.fetchPendingDelivery(+this.userDetails.id).subscribe(
       (deliveryDetails: any) => {
         this.pendingDeliveryDetails = deliveryDetails;
         console.log("Pending Delivery Details --> ", this.pendingDeliveryDetails);
+        this.fetchOrderForPendingDelivery();
       },
       (error) => {
         alert('An error occured while fetching pending delivery information');
