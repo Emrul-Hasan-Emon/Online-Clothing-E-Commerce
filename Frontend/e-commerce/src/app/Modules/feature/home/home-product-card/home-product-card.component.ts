b63be2cf180a535eaa206639809bb3f52f04e125
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-05-20 20:54:33 +0600

    Frontend: Home page design modified and some functionality added.

--- Frontend/e-commerce/src/app/Modules/feature/home/home-product-card/home-product-card.component.ts
+++ Frontend/e-commerce/src/app/Modules/feature/home/home-product-card/home-product-card.component.ts
@@ -1,11 +1,30 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-product-card',
   templateUrl: './home-product-card.component.html',
   styleUrls: ['./home-product-card.component.css']
 })
-export class HomeProductCardComponent {
+export class HomeProductCardComponent implements OnInit {
   @Input() // It will take input from Product Slider Component
   product: any;
+  isDiscount: boolean = false;
+  discountedPrice: number = 0;
+
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if(this.product.Discount) {
+      this.isDiscount = true;
+
+      this.discountedPrice = (this.product.Price * this.product.Discount) / 100;
+    }
+  }
+
+  navigateToProductDetailsPage(productID) {
+    this.router.navigate(['product-details', productID]);
+  }
 }
