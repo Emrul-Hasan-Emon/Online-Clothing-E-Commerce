6776f841cdb85c7969f80dcae63b9ce04cb1576e
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-05-20 22:33:14 +0600

    Frontend: Register Panel updated with validation message.

--- Frontend/e-commerce/src/app/Modules/auth/register/register.component.ts
+++ Frontend/e-commerce/src/app/Modules/auth/register/register.component.ts
@@ -10,11 +10,15 @@ import { AuthService } from 'src/app/Service/auth.service';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   errorMessage: string;
+  isPasswordMatched: boolean = false;
+  password1 = '';
+  password2 = '';
+
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
-      name: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z][a-zA-Z\s]*$/)]), // Name pattern added
       phonenumber: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       dateOfBirth: new FormControl('', Validators.required),
@@ -27,29 +31,34 @@ export class RegisterComponent implements OnInit {
     });
   }
 
-  private resgisterUser(registerUserData: any) {
+  private registerUser(registerUserData: any) {
     this.authService.registerNewUser(registerUserData).subscribe(
       (response) => {
-        alert('Registration successfull');
+        alert('Registration successful');
       },
       (error) => {
         this.errorMessage = error.error;
         console.log(error);
         alert(this.errorMessage);
-        
       }
     )
   }
 
   register() {
-    if (this.registerForm.valid && this.registerForm.get('password').value === this.registerForm.get('confirmPassword').value) {
-      console.log('Register Details ----> ', this.registerForm);
-      const registerUserData = this.registerForm.value;
-      registerUserData.role = 'user';
+    if (this.registerForm.valid) {
+      if (this.registerForm.get('password').value === this.registerForm.get('confirmPassword').value) {
+        console.log('Register Details ----> ', this.registerForm);
+        const registerUserData = this.registerForm.value;
+        registerUserData.role = 'user';
 
-      this.resgisterUser(registerUserData);
+        this.registerUser(registerUserData);
+        this.isPasswordMatched = false;
+        this.registerForm.reset();
+      } else {
+        this.isPasswordMatched = true;
+      }
     } else {
-      alert(`Password didn't match`);
+      alert('The form is not valid');
     }
   }
 }
